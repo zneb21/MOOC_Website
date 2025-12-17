e8fa3eaa36555401998221d371a35ec0b184a75e
mist <ckaristoki@outlook.com>
2025-12-17 11:09:49 +0800

    change to presentation

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -38,7 +38,7 @@ import { useToast } from "@/hooks/use-toast";
 const navLinks = [
   { name: "Home", path: "/" },
   { name: "Courses", path: "/courses" },
-  { name: "Presentations", path: "/presentations" },
+  { name: "Presentation", path: "/presentations" },
   { name: "Games", path: "/games" },
   { name: "About", path: "/about" },
 ];
